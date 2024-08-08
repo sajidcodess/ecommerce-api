93a77f71a7c70e054796cc9e4e56cc3b96445ec6
sajidcodess <muhammadsajidkhan616@gmail.com>
2024-08-09 03:55:00 +0500

    validate email/password

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,29 +1,37 @@
-const mongoose = require('mongoose')
-const { Schema } =  mongoose
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: validator.isEmail,
+      message: (props) => `${props.value} is not a valid email `,
+    },
+  },
+  password: { type: String, required: true, minlength: 6 },
+});
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) {
-    return next()
-  } 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-  next()
-})
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-userSchema.methods.matchPassword = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password)
-}
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-const userModel =   mongoose.model('User', userSchema)
+const userModel = mongoose.model("User", userSchema);
 
-module.exports = userModel
-
-
+module.exports = userModel;
